065ab799b428989fecf3aac1a77bef8d226f0d86
Flash <857558227@qq.com>
2022-09-19 23:45:55 +0800

    fix: 删除variables变量

--- src/store/modules/theme.js
+++ src/store/modules/theme.js
@@ -1,21 +1,18 @@
 // 处理主题色的vuex
 import { setItem, getItem } from '@/utils/storage'
 import { MAIN_COLOR, DEFAULT_COLOR } from '@/constant'
-import variables from '@/assets/styles/variables.module.scss'
 
 const theme = {
   namespaced: true,
   state () {
     return {
-      mainColor: getItem(MAIN_COLOR) || DEFAULT_COLOR,
-      variables
+      mainColor: getItem(MAIN_COLOR) || DEFAULT_COLOR
     }
   },
   mutations: {
     // 设置主题色
     setMainColor (state, newColor) {
       state.mainColor = newColor
-      state.variables.menuBg = newColor
       setItem(MAIN_COLOR, newColor)
     }
   }
