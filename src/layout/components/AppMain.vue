ee70a193850bacb35657ae8f402b4358b396183e
Flash <857558227@qq.com>
2022-09-20 23:51:04 +0800

    perf: 增加路由过渡动效和组件缓存

--- src/layout/components/AppMain.vue
+++ src/layout/components/AppMain.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="app-main">
-    <router-view />
+    <!-- 带有切换动画和组件缓存的router-view -->
+    <!-- 固定写法 -->
+    <router-view v-slot="{ Component, route }">
+      <transition name="fade" mode="out-in">
+        <keep-alive>
+          <component :is="Component" :key="route.path"/>
+        </keep-alive>
+      </transition>
+    </router-view>
   </div>
 </template>
 
@@ -18,7 +26,14 @@ watch(
   route,
   (to, from) => {
     if (isTag(to.path)) {
-      const { fullPath, path, meta, name, params, query } = to
+      const {
+        fullPath,
+        path,
+        meta,
+        name,
+        params,
+        query
+      } = to
       store.commit('app/addTag', {
         fullPath,
         path,
@@ -37,10 +52,24 @@ watch(
 <style lang="scss" scoped>
 .app-main {
   width: 100%;
-  min-height: calc(100vh - 50px);
+  min-height: calc(100vh - 50px - 43px);
   position: relative;
   box-sizing: border-box;
-  padding: 61px 20px 20px 20px;
+  padding: 104px 20px 20px 20px;
   overflow: hidden;
+}
+
+.fade-enter-from, .fade-leave-to {
+  transform: translateX(-20px);
+  opacity: 0;
+}
+
+.fade-enter-active, .fade-leave-active {
+  transition: all 0.3s;
+}
+
+.fade-enter-to, .fade-leave-from {
+  transform: none;
+  opacity: 1;
 }
 </style>
